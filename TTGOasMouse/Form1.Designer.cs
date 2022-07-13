7682f2656c8bfa980809ed087ab101205ab8da61
Tashfi Nowroz <tnowroz@gmail.com>
2022-07-14 03:23:07 +0600

    Initial Commit

---
+++ TTGOasMouse/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TTGOasMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.LimeGreen;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select COM Port (TTGO Bluetooth)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BAUD Speed";
+            // 
+            // textBox1
+            // 
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(0, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(419, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Touch Control";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(392, 149);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            //
+            //
+            this.textBox1.BackColor = System.Drawing.Color.ForestGreen;
+            this.textBox1.Location = new System.Drawing.Point(242, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(419, 273);
+            this.ControlBox = false;
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TTGOasMouse/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace TTGOasMouse {
+
+    public partial class Form1 : Form {
+        public Thread readThread;
+        public String ReadSerialVal;
+        static bool _continue = false;
+        public static SerialPort _serialPort = new SerialPort();
+
+
+        public Form1() {
+            InitializeComponent();
+            //control = richTextBox1;
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e) {
+            comboBox1.Items.Clear();
+            foreach (string s in SerialPort.GetPortNames()) {
+                comboBox1.Items.Add(s);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            VirtualMouse.MoveTo(1000, 0);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            if (!_continue) {
+
+                StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+
+
+
+                // Allow the user to set the appropriate properties.
+                _serialPort.PortName = comboBox1.Text;
+                _serialPort.BaudRate = int.Parse(textBox1.Text);
+
+                // Set the read/write timeouts
+                _serialPort.ReadTimeout = 500;
+                _serialPort.WriteTimeout = 500;
+                try {
+                    _serialPort.Open();
+                    button1.BackColor = Color.Red;
+                    _continue = true;
+                    button1.Text = "Close Port";
+                    readThread = new Thread(Read);
+                    readThread.Start();
+                } catch (Exception eee) {
+                    MessageBox.Show(eee.Message);
+                }
+            }  else {
+                button1.BackColor = Color.Yellow;
+                button1.Text = "Open Port";
+                //readThread.Abort();
+                _serialPort.Close();
+                _continue = false;
+                
+            }
+
+        }
+
+        private void UpdateLabel(string newText) {
+            if ((newText.ToLower()).Contains("clicked")) {
+                richTextBox1.AppendText("Click!\n");
+            } else {
+                String x = newText.Split(',')[0];
+                String y = newText.Split(',')[1];
+                richTextBox1.AppendText("X: " + x + "   Y: " + y);
+                VirtualMouse.MoveTo(Int16.Parse(x), Int16.Parse(y));
+            }
+            richTextBox1.ScrollToCaret();
+        }
+
+        public delegate void ShowSerialData(String r);
+        private void Read() {
+            while (_continue) {
+                try {
+
+                    ReadSerialVal = _serialPort.ReadLine();
+                    if (richTextBox1.InvokeRequired) {
+                        ShowSerialData ssd = UpdateLabel;
+                        Invoke(ssd, ReadSerialVal);
+                    } else {
+                        richTextBox1.AppendText(ReadSerialVal);
+                        richTextBox1.ScrollToCaret();
+                    }
+
+                } catch (TimeoutException) {
+
+                } catch (Exception ex) {
+                    break;
+                } 
+            }
+        }
+   
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+
+    public static class VirtualMouse {
+
+
+        [DllImport("user32.dll")]
+
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+
+        public static float Remap(float value, float from1, float to1, float from2, float to2) {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+
+        public static void Move(int xDelta, int yDelta) {
+            mouse_event(MOUSEEVENTF_MOVE, xDelta, yDelta, 0, 0);
+        }
+        public static void MoveTo(int x, int y) {
+            float min = 0;
+            float max = 65535;
+
+            int mappedX = (int)Remap(x, 0.0f, 1920.0f, min, max);
+            int mappedY = (int)Remap(y, 0.0f, 1080.0f, min, max);
+            mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, mappedX, mappedY, 0, 0);
+        }
+        public static void LeftClick() {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void LeftDown() {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void LeftUp() {
+            mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void RightClick() {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void RightDown() {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void RightUp() {
+            mouse_event(MOUSEEVENTF_RIGHTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+    }
+
+
+
+}
